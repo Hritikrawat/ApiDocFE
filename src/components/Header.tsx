d6240565c844d60e47972bdb97654bbb33f52ce8
Ritik Rawat <88231522+Hritikrawat@users.noreply.github.com>
2025-07-24 17:55:15 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import { FileText, Code } from 'lucide-react';
-import Saltoris from './images/Saltoris.jpg'
+import { FileText } from 'lucide-react';
+import Saltoris from './images/Saltoris.jpg';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-white text-white p-4 shadow-md">
+    <header className="bg-white text-white p-3 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-2">
           <FileText size={24} />
-          <h1 className="text-xl font-bold text-blue-600">API Documentation Generator</h1>
+          <h1 className="text-lg font-bold text-blue-600">API Documentation Generator</h1>
         </div>
-        {/* <div className="flex items-center space-x-2"> */}
-          {/* <Code size={20} /> */}
-          {/* <span className="text-sm">Build beautiful API docs</span> */}
-        {/* </div> */}
 
-        <div className="flex items-center justify-center w-40">
+        <div className="flex items-center justify-center w-14 h-14">
           <img 
             src={Saltoris}
-            alt="Coding workspace" 
-            // className="w-40 h-40 object-cover rounded-md shadow-sm"
+            alt="Coding workspace"
+            className="w-14 h-14 object-cover rounded-md shadow-sm"
           />
         </div>
       </div>
